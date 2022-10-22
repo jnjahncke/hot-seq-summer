64e48acacdf633308ab0eb499f503fd2e20ce249
Jennifer Jahncke <jennifer.jahncke@gmail.com>
2022-10-22 10:35:00 -0400

    domains functions

---
+++ domain_comp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Input: 2 lists
+#	List of enriched domains in diff exp genes of species1
+#	List of enriched domains in diff exp genes of species2
+# 
+# Output: which domains are enriched in both lists?
---
+++ enriched_domains.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Inputs:
+#	1: dictionary containing genes, p-value, log fold change
+#	2: dictionary containing genes, domains
+#
+# Calculate:
+#	Which genes are upregulated (or downregulated)?
+#	For those genes, which domains are enriched?
+#
+# Output: list of enriched domains
