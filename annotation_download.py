116aef077525b148218ea2f86f84361c10b03e86
student <student@info-02.cshl.edu>
2022-10-22 20:18:10 -0400

    adding functional pfam specific annotation_download.py

--- annotation_download.py
+++ annotation_download.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-import sys, os
-import pandas as pd
 from pybiomart import Dataset
+import pandas as pd
 import numpy as np
+import sys, os
+import math
 import re
-# chmod +x annotation_download.py
 
 
 
-### Returns 
+### Returns seaparate dictionaries: key=Ensembl stable geneID values=list of pfam IDs for protein domains ###
 
 
 
-# List names and atrributes to modify the query for each species
-# Common names: zebra fish, rat, chicken
-speciesList =  ['drerio', 'rnorvegicus', 'ggallus']
+def species_domain_dict():
 
-# Pass in arguments later?
-def annot_species_dict():
+  # Species list to modify the query to Ensembl Genes 108 database--common names: human, mouse, yeast
+  #speciesList =  ['hsapiens', 'mmusculus', 'scerevisiae']
+  speciesList =  ['scerevisiae']
 
   # Run a query for each species
   for species in speciesList:
@@ -25,29 +24,23 @@ def annot_species_dict():
     # A Dataset instance can be constructed directly if the name of the dataset and the url of the host are known
     dataset = Dataset(name=f"{species}_gene_ensembl", host='http://www.ensembl.org')
 
+    # Biomart server query returns a dataframe of 2 columns: 'Gene stable ID' and 'Pfam ID'
+    dfID = dataset.query(attributes=['ensembl_gene_id', 'pfam'])
+    # Remove nonvalues [nan] from dataframe
+    dfIDs = dfID.dropna()
 
-    # Query the biomart server returns a df of 3 columns: index, 'Gene stable ID' and 'Pfam ID'
-    dfIDs = dataset.query(attributes=['ensembl_gene_id', 'pfam'])
+    # Pull the dataframe columns into a list of lists (gene ID and Pfam pairs)
+    idArray = dfIDs.to_numpy()   
 
-
-    # Pull the second and third columns into a list of lists (gene ID and Pfam pairs)
-    # Remove nan values doesn't work yet    
-    idArray = dfIDs.to_numpy()
-    #idArray = idArrayNAN[~np.isnan(idArrayNAN)]
-    
+ 
     # Parse array into a dictionary 
-    # Will creat one for each species
     annotationDict = {}
     for idList in idArray:
       geneID = idList[0]
       pfamID = idList[1]
       
-      # Skip over genes with no pfam entry
-      if pfamID == 'nan':
-        continue			### Still getting empty Pfam entries added to list with "" or "nan" ###
-      
       # Add pfam ID to an existing gene ID key
-      elif geneID in annotationDict:
+      if geneID in annotationDict:
         annotationDict[geneID].append(pfamID)
       
       # For a new key, establish a list and add the pfam
@@ -55,14 +48,14 @@ def annot_species_dict():
         annotationDict[geneID] = []
         annotationDict[geneID].append(pfamID)
 
-
+  # A separate dictionary is returned for each species
   return(annotationDict)
 
 
 
 def main ():
  #Remove print after troubleshooting
- print(annot_species_dict())
+ print(species_domain_dict())
 
 
 if __name__ == '__main__':
