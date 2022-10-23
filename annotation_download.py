385d499df04c04b98aa13a379882545d925589ad
student <student@info-02.cshl.edu>
2022-10-22 21:16:00 -0400

    adding generic annotation_download pfam and go_id working

--- annotation_download.py
+++ annotation_download.py
@@ -8,45 +8,38 @@ import re
 
 
 
-### Returns seaparate dictionaries: key=Ensembl stable geneID values=list of pfam IDs for protein domains ###
+### Returns separate dictionaries: key=Ensembl stable geneID values=list of pfam IDs for protein domains ###
 
 
 
-def species_domain_dict():
+def species_annotation_dict(species, annotation_type):
 
-  # Species list to modify the query to Ensembl Genes 108 database--common names: human, mouse, yeast
-  #speciesList =  ['hsapiens', 'mmusculus', 'scerevisiae']
-  speciesList =  ['scerevisiae']
+  # A Dataset instance can be constructed directly if the name of the dataset and the url of the host are know
+  dataset = Dataset(name=f"{species}_gene_ensembl", host='http://www.ensembl.org')
 
-  # Run a query for each species
-  for species in speciesList:
+  # Biomart server query returns a dataframe of 2 columns: 'Gene stable ID' and 'Pfam ID'
+  datasetDF = dataset.query(attributes=['ensembl_gene_id', annotation_type])
+  # Remove nonvalues [nan] from dataframe
+  datasetDF_nonan = datasetDF.dropna()
 
-    # A Dataset instance can be constructed directly if the name of the dataset and the url of the host are known
-    dataset = Dataset(name=f"{species}_gene_ensembl", host='http://www.ensembl.org')
-
-    # Biomart server query returns a dataframe of 2 columns: 'Gene stable ID' and 'Pfam ID'
-    dfID = dataset.query(attributes=['ensembl_gene_id', 'pfam'])
-    # Remove nonvalues [nan] from dataframe
-    dfIDs = dfID.dropna()
-
-    # Pull the dataframe columns into a list of lists (gene ID and Pfam pairs)
-    idArray = dfIDs.to_numpy()   
+  # Pull the dataframe columns into a list of lists (gene ID and Pfam pairs)
+  idArray = datasetDF_nonan.to_numpy()   
 
  
-    # Parse array into a dictionary 
-    annotationDict = {}
-    for idList in idArray:
-      geneID = idList[0]
-      pfamID = idList[1]
+  # Parse array into a dictionary 
+  annotationDict = {}
+  for idList in idArray:
+    geneID = idList[0]
+    annotation = idList[1]
       
-      # Add pfam ID to an existing gene ID key
-      if geneID in annotationDict:
-        annotationDict[geneID].append(pfamID)
+    # Add pfam ID to an existing gene ID key
+    if geneID in annotationDict:
+      annotationDict[geneID].append(annotation)
       
-      # For a new key, establish a list and add the pfam
-      else:
-        annotationDict[geneID] = []
-        annotationDict[geneID].append(pfamID)
+    # For a new key, establish a list and add the pfam
+    else:
+      annotationDict[geneID] = []
+      annotationDict[geneID].append(annotation)
 
   # A separate dictionary is returned for each species
   return(annotationDict)
@@ -54,8 +47,25 @@ def species_domain_dict():
 
 
 def main ():
- #Remove print after troubleshooting
- print(species_domain_dict())
+  # Species options to modify the query to Ensembl Genes 108 database: hsapiens, mmusculus, scerevisiae
+  species = sys.argv[1].lower()
+  # Annotation options for Ensembl query: pfam or go_id
+  annotation_type = sys.argv[2].lower()
+
+  # Call the function to make the dictionary
+  dict_to_write = species_annotation_dict(species, annotation_type)  
+  #print(dict_to_write)
+ 
+  # Because dictionaries take minutes to load, they are written to a file to avoid re-downloading 
+  annotationDF_txt = open(f"{species}_{annotation_type}.txt", "w")
+  
+  # Write each key to a line in the file, note the new line character
+  for gene_id in dict_to_write:
+   annotationDF_txt.write(f"{gene_id}\t{dict_to_write[gene_id]}\n")
+  
+  # Close file after writing finished
+  annotationDF_txt.close()
+
 
 
 if __name__ == '__main__':
