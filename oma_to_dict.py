b15f8463f4d93893b9e2bebec1998f90b1fb31b2
student <student@info-05.cshl.edu>
2022-10-22 21:21:21 -0400

    updating code for adding oma groups to dict

--- oma_to_dict.py
+++ oma_to_dict.py
@@ -2,11 +2,9 @@
 
 import sys
 
-def oma_dict():
-	oma_file = sys.argv[1]
-	oma_ens_file = sys.argv[2]
+def make_oma_dict():
+	oma_file = sys.argv[3]
 	oma_dict = {}
-	o2e_dict = {}
 
 ## create a dictionary of oma accession numbers belonging to each oma group.
 ## key:value pairs are oma_group:[list of genes]
@@ -18,12 +16,16 @@ def oma_dict():
 				line= line.rstrip()
 				group_list = line.split('\t')
 				oma_dict[group_list[0]] = group_list[2:]
-
 	print(f'''
 Dictionary of genes belonging to oma groups complete!
 key:value pairs are oma_group:[list of genes]
 ''')
+	return(oma_dict)
 
+
+def omaID_to_ensID():
+	oma_ens_file = sys.argv[4]
+	o2e_dict = {}
 ## Read in the oma to ensemble accession number table to a dictionary
 	with open(oma_ens_file, 'r') as obj:
 		for line in obj:
@@ -39,15 +41,15 @@ Oma to ensemble accession dict done!
 key:values are oma:ens
 ''')
 
-	orthos_ensID = {}
+#	orthos_ensID = {}
 
-	for group in oma_dict:
-		gene_list = oma_dict[group]
-		orthos_ensID[group] = []
-		for gene in gene_list:
-			orthos_ensID[group].append(o2e_dict)
+#	for group in oma_dict:
+#		gene_list = oma_dict[group]
+#		orthos_ensID[group] = []
+#		for gene in gene_list:
+#			orthos_ensID[group].append(o2e_dict)
 
-	print(orthos_ensID)
+#	print(orthos_ensID)
 
 #def main():
 #print(f'File meant to be run within another script')
