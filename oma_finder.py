f531b85f92e6c32a123ca21805fd04683915fd84
student <student@info-05.cshl.edu>
2022-10-23 17:25:20 -0400

    buggy oma_finder code

--- oma_finder.py
+++ oma_finder.py
@@ -6,28 +6,30 @@ from rnaseq_to_dict import *
 
 
 ## define the function to pull a list of significantly up and down regulated genes from a dictionary of DEG data
-## input deg_data must be in the format: {ensemble_ID: {'logFC':value , 'pvalue' : value}}
+## Dictionary of DEG data must be in the format: {file : {gene : [logFC, pvalue]}}
+## call dictionary[file]
 def deg_list(deg_data):
 	genes_up = []
 	genes_down = []
 	for gene in deg_data:
-		if deg_data[gene]['pvalue'] < 0.05:
-			if deg_data[gene]['logFC'] > 0:
+		if deg_data[gene][1] < 0.05:
+			if deg_data[gene][0] > 0:
 				genes_up.append(gene)
-			if deg_data[gene]['logFC'] < 0:
+			if deg_data[gene][0] < 0:
 				genes_down.append(gene)
 	return genes_up, genes_down
 
 ## define the function to convert a list of ensemble IDs to oma IDs
+## ens_list should be a list of ensemble IDs for the up or down regulated genes in a single species
 def convert_to_oma(ens_list):
 	oma_list = []
-	for oma_ID in o2e_dict:
-		for gene in ens_list:
+	for gene in ens_list:
+		for oma_ID in o2e_dict:
 			if gene == o2e_dict[oma_ID]:
 				oma_list.append(oma_ID)
 	return(oma_list)
 
-## define the functino for taking a list of oma ID and grabbing the oma_group they belong to, storing them in a set
+## define the function for taking a list of oma ID and grabbing the oma_group they belong to, storing them in a set
 def find_oma_groups(oma_list):
 	oma_groups = set()
 	for oma_ID in oma_dict:
@@ -40,17 +42,12 @@ def find_oma_groups(oma_list):
 sp1_file = sys.argv[1]
 sp2_file = sys.argv[2]
 sp_file_list = [sp1_file, sp2_file]
-sp1 = 'YEAST'
-sp2 = 'MOUSE'
 
 ## put deg data into dictionary for 2 sp
-sp1_data, sp2_data = rnaseqs_to_dict(sp_file_list)
-
-print(sp1_data)
-print(sp2_data)
+sp_data = rnaseqs_to_dict(sp_file_list)
 
 print(f'''
-The deg data have been put into dictionaries''')
+The deg data have been put into a dictionary''')
 
 ## read in the oma groups into a dictionary
 ## oma_dict format: omaID : oma_group
@@ -59,26 +56,43 @@ oma_dict = make_oma_dict()
 ## read in the oma ID to ens ID conversion dict
 o2e_dict = omaID_to_ensID()
 
-## create a list of up and down regulated genes for each species using the deg_list function defined above, store the lists in new variables
-up1_ensID, down1_ensID = deg_list(sp1_data)
-up2_ensID, down2_ensID = deg_list(sp2_data)
+## create a list of up and down regulated genes for each species using the deg_list function defined above, store the lists in new variable
 
-print(f'up1 ens : {up1_ensID}')
-print(f'up2 ens : {up2_ensID}')
+exp_data ={}
+
+for file in sp_data:
+	exp_data[file] = {}
+	exp_data[file]['up'] = []
+	exp_data[file]['down'] = []
+	exp_data[file]['up'],exp_data[file]['down'] = deg_list(sp_data[file])
+
+print(f'Expression data: {exp_data}')
 
 print(f'''
 Up and down reg genes have been found for both species
 ''')
 
+up_exp_omaID = {}
+down_exp_omaID = {}
+
+
 ## convert the list of ensemble IDs to a list of oma IDs using the function defined above
-up1_omaID = convert_to_oma(up1_ensID)
-#down1_omaID = convert_to_oma(down1_ensID)
-up2_omaID = convert_to_oma(up2_ensID)
-#down2_omaID = convert_to_oma(down2_ensID)
-print(f'up1 omas : {up1_omaID}')
-print(f'up2 omas : {up2_omaID}')
+for file in exp_data:
+	up_exp_omaID[file] = {}
+	up_exp_omaID[file]['up'] = []	
+	up_ensIDs = exp_data[file]['up']
+	up_exp_omaID[file]['up'] = convert_to_oma(up_ensIDs)
+	
+	down_exp_omaID[file] = {}
+	down_exp_omaID[file]['down'] = []
+	down_ensIDs = exp_data[file]['down']
+	down_exp_omaID[file]['down'] = convert_to_oma(down_ensIDs)
+
 
 ## find the oma groups for each deg list using the function defined above
+
+
+
 up1_oma_groups = find_oma_groups(up1_omaID)
 #down1_oma_groups = find_oma_groups(down1_omaID)
 up2_oma_groups = find_oma_groups(up2_omaID)
