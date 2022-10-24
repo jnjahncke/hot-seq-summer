8c81d5ccbcb9b99083672fe6f4ccc15aa7ded18a
student <rkellemeyer@stowers.org>
2022-10-24 11:36:21 -0400

    final GSEA scripts

--- GSEA_final.py
+++ GSEA_final.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+###Gene set enrichment analysis KRL_10-24-22
+
 import pandas as pd
 from pandas import DataFrame as df
 import gseapy as gp
@@ -10,19 +13,18 @@ from gseapy import biomart
 from shared_functions import *
     
 def main():
-    expr_dict = rnaseqs_to_dict(["RawData/HUMAN_genes.txt"])
+    expr_dict = rnaseqs_to_dict(["RawData/HUMAN_gsea.txt","RawData/mus_rna_seq_final.txt", "RawData/yeast_degs.txt"])
   
-    run_gsea(expr_dict["RawData/HUMAN_genes.txt"], "hsapiens", "human" )
-    #run_gsea(expr_dict["RawData/mus_rna_seq_final.txt"],"mmusculus", )
-    #run_gsea(expr_dict["RawData/yeast_degs.txt"],"scerevisiae" )
+    run_gsea(expr_dict["RawData/HUMAN_gsea.txt"], "hsapiens", "human" )
+    run_gsea(expr_dict["RawData/mus_rna_seq_final.txt"],"mmusculus","mouse" )
+    #run_gsea(expr_dict["RawData/yeast_degs.txt"],"scerevisiae", "Yeast")
   
 def run_gsea(expr_dict, species, common_name):
     gene_list = {}
     for gene in expr_dict:
-        gene_list[gene] = float(expr_dict[gene][0])	###########################################fix line###################
-    #glist = dict(sorted(gene_list.items(), reverse = True, key=lambda item: item[1])).keys()
-    glist = sorted(gene_list, key = gene_list.get)
-  
+        gene_list[gene] = float(expr_dict[gene][0])	
+    glist = sorted(gene_list, key = gene_list.get, reverse = True)
+    
     #call species_annotation_df function and decalare species and ensemble data types
     x=species_annotation_df(species, 'external_gene_name')
   
@@ -40,15 +42,15 @@ def run_gsea(expr_dict, species, common_name):
     
     #Run enrichr wrapper on new elist with declared gene set and species name
     enr = gp.enrichr(gene_list=elist, 
-                      gene_sets=['GO_Cellular_Component_2021','GO_Molecular_Function_2021'],
-                      organism=common_name, #set organism to the one you want (human, mouse, yeast)
+                      gene_sets=['GO_Molecular_Function_2021', 'GO_Biological_Process_2021', 'Pfam_Domains_2019'],
+                      organism=common_name,
                       outdir=None, #doesn't save the output to your machine               
                      )
     enr.results.to_csv(f'{species}_gsearesults.tsv', sep = '\t')    
     
     #plot enriched data sets
-    barplot(enr.res2d,title='GO_Molecular_Function_2021', ofname='barplot.pdf')
-    dotplot(enr.res2d,title='GO_Molecular_Function_2021', ofname='dotplot.png')
+    barplot(enr.res2d,title='Gene Set Enrichment Analysis', ofname=f'{common_name}_barplot.png')
+    dotplot(enr.res2d,title='Gene Set Enrichment Analysis', ofname=f'{common_name}_dotplot.png')
     
 if __name__ == "__main__":
     main()
